b1daf111a60924d3e43ed3f83d31a7c69f020273
ethanmjansen <ethanmjansen@users.noreply.github.com>
2020-06-04 17:10:36 -0600

    MVP Day4

--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -13,3 +13,17 @@ def f(x):
 
 # Your code here
 
+sums = {}
+diffs = {}
+
+for i in q:
+    for j in q:
+
+        sum = f(i) + f(j)
+
+        if sum in sums:
+            sums[sum].append((i, j))
+        else:
+            sums[sum] = [(i, j)]
+
+print(sums)
